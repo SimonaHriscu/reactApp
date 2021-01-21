26027963930409e0f512daf7b030132fc5990e34
SimonaHriscu <simona_hriscu@yahoo.com>
2021-01-21 13:51:14 +0100

    useOnScreenObserver fct component added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
+import useOnScreen from "components/useOnScreenObserver";
 import Movies from "pages/Movies";
 import Section from "pages/Section";
 
@@ -17,24 +18,6 @@ const useStyles = makeStyles({
   },
 });
 
-function useOnScreen(options) {
-  const [ref, setRef] = useState(null);
-  const [visible, setVisible] = useState(false);
-  useEffect(() => {
-    const observer = new IntersectionObserver(([entry]) => {
-      setVisible(entry.isIntersecting);
-    }, options);
-    if (ref) {
-      observer.observe(ref);
-    }
-    return () => {
-      if (ref) {
-        observer.unobserve(ref);
-      }
-    };
-  }, [ref, options]);
-  return [setRef, visible];
-}
 function App() {
   const [setRefSection1, visibleSection1] = useOnScreen({ threshold: 0.5 });
   const [setRefSection2, visibleSection2] = useOnScreen({ threshold: 0.5 });
@@ -48,21 +31,21 @@ function App() {
         reference={setRefSection1}
         style={{ backgroundColor: visibleSection1 ? "#ffdf91" : "#557174" }}
       >
-        {visibleSection1 ? <h1>Hey, I am on the screen!</h1> : ""}
+        {visibleSection1 && <h1>Hey, I am on the screen!</h1>}
       </Section>
       <Section
         className={classes.section}
         reference={setRefSection2}
         style={{ backgroundColor: visibleSection2 ? "#f8f1f1" : "#48426d" }}
       >
-        {visibleSection2 ? <h1>Hey, I am section 2!</h1> : ""}
+        {visibleSection2 && <h1>Hey, I am section 2!</h1>}
       </Section>
       <Section
         className={classes.section}
         reference={setRefSection3}
         style={{ backgroundColor: visibleSection3 ? "#ff884b" : "#e1d89f" }}
       >
-        {visibleSection3 ? <h1>Hey, I am section 3!</h1> : ""}
+        {visibleSection3 && <h1>Hey, I am section 3!</h1>}
       </Section>
     </div>
   );
