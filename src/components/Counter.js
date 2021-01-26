c9042364cb9a96115ad3a708346b7b85cfae30c5
Simona <see.moona@yahoo.com>
2021-01-26 14:21:18 +0100

    fetch data from api/ axios

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,43 +1,56 @@
 import React, { useState, useEffect } from 'react';
-
-// const fetchRandomData = async () => {
-//   fetch('https://randomuser.me/api')
-//     .then((res) => res.json())
-//     .then((data) => {
-//       console.log(JSON.stringify(data));
-//       return JSON.stringify(data);
-//     })
-//     .catch((err) => {
-//       console.error(err);
-//     });
-// };
+import axios from 'axios';
 
 const Counter = () => {
   //https://randomuser.me/api
 
   const [counter, setCounter] = useState(0);
   const [randomString, setRandomString] = useState('');
+  const [randomData, setRandomData] = useState('');
+  const [on, setOn] = useState(false);
+
   const handleIncrement = () => {
     setCounter(counter + 1);
   };
   const handleDecrement = () => {
     setCounter(counter - 1);
   };
+
+  //  both fetch and axios just for practice
+  //   use fetch() when building relatively small applications and make use of Axios when building large applications for scalability reasons.
+  //   for data to appear directly on load
   useEffect(() => {
     fetch('https://randomuser.me/api')
       .then((res) => res.json())
       .then((data) => {
-        console.log('This is your data', data);
         setRandomString(JSON.stringify(data));
       });
+    //   for the data to appear on click
+    axios.get('https://randomuser.me/api').then(({ data }) => {
+      setRandomData(data);
+    });
   }, []);
+
+  const handleData = () => {
+    setOn(!on);
+  };
+  // console.log(randomData.results);
   return (
     <>
       <button onClick={() => handleIncrement()}>Increment</button>
       <p>{counter}</p>
       <button onClick={() => handleDecrement()}>Decrement</button>
       <p>{randomString}</p>
-      {/* <button onClick={() => fetchRandomData()}>Fetch Data</button> */}
+      <button onClick={() => handleData()}>Fetch Data</button>
+      {on &&
+        randomData.results.map((item, i) => {
+          return (
+            <div key={i}>
+              <p>{item.name.first}</p>
+              <img src={item.picture.medium} />
+            </div>
+          );
+        })}
     </>
   );
 };
